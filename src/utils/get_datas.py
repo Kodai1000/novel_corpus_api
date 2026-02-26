33ddbc89a80799f5bc82daae4e2906000ef33406
Kodai.T <fvbcj53523@yahoo.co.jp>
2026-02-27 01:51:28 +0900

    use both contains and like function in get_datas

--- src/utils/get_datas.py
+++ src/utils/get_datas.py
@@ -60,10 +60,12 @@ def search_word_in_co_occurrence(db, identifier, keyword):
                     co_occurrence.head_word.contains(katakana),
                     co_occurrence.target_word.contains(keyword),
                     co_occurrence.target_reading.contains(katakana),
-                    keyword.contains(co_occurrence.head_word),
-                    katakana.contains(co_occurrence.head_word),
-                    keyword.contains(co_occurrence.target_word),
-                    katakana.contains(co_occurrence.target_reading),
+                    
+                    co_occurrence.head_word.like(keyword),
+                    co_occurrence.head_word.like(katakana),
+                    co_occurrence.target_word.like(keyword),
+                    co_occurrence.target_reading.like(katakana),
+                    
                 ),
             )
         )
